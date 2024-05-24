c461f1d924f92ee5524790f5926e65853f3c524a
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2024-05-23 22:51:50 -0700

    RegisterTeam update

--- mainApp/models.py
+++ mainApp/models.py
@@ -29,25 +29,9 @@ class UsAbout(CoreModel):
         return self.title
 
 
-DIRECTION_CHOICES = [
-    ('Onlayn do\'kon', 'Onlayn do\'kon'),
-    ('Consulting', 'Consulting'),
-    ('O\'quv markaz', 'O\'quv markaz'),
-    ('Onlayn sotuv (kurs vhk)', 'Onlayn sotuv (kurs vhk)'),
-    ('Universitet maktab', 'Universitet maktab'),
-    ('Mebel (sotish va ishlab chiqarish)', 'Mebel (sotish va ishlab chiqarish)'),
-    ('Ishlab chiqarish', 'Ishlab chiqarish'),
-    ('Xizmat ko\'rsatish', 'Xizmat ko\'rsatish'),
-    ('Tur firma', 'Tur firma'),
-    ('Sotuv oflayn', 'Sotuv oflayn'),
-
-]
-
-
 class RegisterTeam(CoreModel):
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15)
-    direction = models.CharField(max_length=100, choices=DIRECTION_CHOICES, default='Onlayn do\'kon')
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
